04ee943d035e5d8678c5da06b34c853e8d9ba1b2
loperkoper <81435854+loperkoper@users.noreply.github.com>
2021-04-20 23:33:34 +0430

    Update script.py

--- script.py
+++ script.py
@@ -421,7 +421,255 @@ def first():
     sleep(3)
 first()
 
+def reset():
+    ##go to first page
+    mouse.position = (142, 642)
+    mouse.click(Button.left, 1)
+    sleep(3)
+    ##max tor
+    mouse.position = (975, 11)
+    mouse.click(Button.left, 1)
+    mouse.position = (1339, 40)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##close tor
+    mouse.position = (990, 10)
+    mouse.click(Button.left, 1)
+    mouse.position = (1355, 9)
+    mouse.click(Button.left, 1)
+    sleep(3)
+    ##close Terminal
+    mouse.position = (570, 408)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    mouse.position = (999, 94)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+
+
+    ##scond page
+    mouse.position = (190, 642)
+    mouse.click(Button.left, 1)
+    sleep(3)
+    ##max tor
+    mouse.position = (975, 11)
+    mouse.click(Button.left, 1)
+    mouse.position = (1339, 40)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##close tor
+    mouse.position = (990, 10)
+    mouse.click(Button.left, 1)
+    mouse.position = (1355, 9)
+    mouse.click(Button.left, 1)
+    sleep(3)
+    ##close Terminal
+    mouse.position = (570, 408)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    mouse.position = (999, 94)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+
+    ##third page
+    mouse.position = (238, 642)
+    mouse.click(Button.left, 1)
+    sleep(3)
+    ##max tor
+    mouse.position = (975, 11)
+    mouse.click(Button.left, 1)
+    mouse.position = (1339, 40)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##close tor
+    mouse.position = (990, 10)
+    mouse.click(Button.left, 1)
+    mouse.position = (1355, 9)
+    mouse.click(Button.left, 1)
+    sleep(3)
+    ##close Terminal
+    mouse.position = (570, 408)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    mouse.position = (999, 94)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+
+
+    ##fourth page
+    mouse.position = (280, 642)
+    mouse.click(Button.left, 1)
+    sleep(3)
+    ##max tor
+    mouse.position = (975, 11)
+    mouse.click(Button.left, 1)
+    mouse.position = (1339, 40)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##close tor
+    mouse.position = (990, 10)
+    mouse.click(Button.left, 1)
+    mouse.position = (1355, 9)
+    mouse.click(Button.left, 1)
+    sleep(3)
+    ##close Terminal
+    mouse.position = (570, 408)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    mouse.position = (999, 94)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+
+
+
+    ##go to first page
+    mouse.position = (142, 642)
+    mouse.click(Button.left, 1)
+    terminal()
+    ##type
+    keyboard.type('cd /root/Desktop/tor-browser_en-US/Browser && ./firefox')
+    sleep(0.5)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    sleep(5)
+    ##click on configure
+    mouse.position = (760, 297)
+    mouse.click(Button.left, 1)
+    sleep(1)
+    ##click on connect
+    mouse.position = (914, 556)
+    mouse.click(Button.left, 1)
+
+    url = ['gestyy.com/ey2pMV','gestyy.com/eyR2QO','gestyy.com/eyETAR']
+    ##max tor
+    mouse.position = (975, 11)
+    mouse.click(Button.left, 1)
+    mouse.position = (1339, 40)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##click search bar
+    mouse.position = (365, 72)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##type url
+    c = random.choice(url)
+    keyboard.type(c)
+    sleep(0.5)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    sleep(1)
+    ##go to second page
+    mouse.position = (190, 642)
+    mouse.click(Button.left, 1)  
+    sleep(1.5)
+    terminal()
+    ##click on terminal
+    mouse.position = (570, 408)
+    mouse.click(Button.left, 1)
+    sleep(1)
+    ##write command
+    keyboard.type('cd /root/Desktop/tor1/tor-browser_en-US/Browser/ && ./firefox')
+    sleep(0.5)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    sleep(1)
+    sleep(5) 
+    sleep(15)
+    ##max tor
+    mouse.position = (975, 11)
+    mouse.click(Button.left, 1)
+    mouse.position = (1339, 40)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##click search bar
+    mouse.position = (365, 72)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##type url
+    c = random.choice(url)
+    keyboard.type(c)
+    sleep(0.5)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    sleep(1)
+
+    ##go to third page
+    mouse.position = (238, 642)
+    mouse.click(Button.left, 1)  
+    sleep(1.5)
+    terminal()
+    ##click on terminal
+    mouse.position = (570, 408)
+    mouse.click(Button.left, 1)
+    sleep(1)
+    ##write command
+    keyboard.type('cd /root/Desktop/tor2/tor-browser_en-US/Browser/ && ./firefox')
+    sleep(0.5)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    sleep(1)
+    sleep(5) 
+    sleep(12)
+    ##max tor
+    mouse.position = (975, 11)
+    mouse.click(Button.left, 1)
+    mouse.position = (1339, 40)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##click search bar
+    mouse.position = (365, 72)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##type url
+    c = random.choice(url)
+    keyboard.type(c)
+    sleep(0.5)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    sleep(1)
+
+    ##go to fourth page
+    mouse.position = (280, 642)
+    mouse.click(Button.left, 1)  
+    sleep(2)
+    terminal()
+    ##click on terminal
+    mouse.position = (570, 408)
+    mouse.click(Button.left, 1)
+    sleep(1)
+    ##write command
+    keyboard.type('cd /root/Desktop/tor3/tor-browser_en-US/Browser/ && ./firefox')
+    sleep(0.5)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    sleep(1)
+    sleep(5) 
+    sleep(12)
+    ##max tor
+    mouse.position = (975, 11)
+    mouse.click(Button.left, 1)
+    mouse.position = (1339, 40)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##click search bar
+    mouse.position = (365, 72)
+    mouse.click(Button.left, 1)
+    sleep(0.5)
+    ##type url
+    c = random.choice(url)
+    keyboard.type(c)
+    sleep(0.5)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    sleep(3)
+
+i = 0
 while True:
+   i = i+1
+   if i == 20:
+    reset()
+    i = 0
+   else:
     url = ['gestyy.com/ey2pMV','gestyy.com/eyR2QO','gestyy.com/eyETAR']
     ##go to first page
     mouse.position = (142, 642)
@@ -587,6 +835,7 @@ while True:
     mouse.position = (365, 72)
     mouse.click(Button.left, 1)
     sleep(0.5)
+
     ##type url
     c = random.choice(url)
     keyboard.type(c)
